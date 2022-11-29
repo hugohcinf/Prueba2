efeebe8f46de806b4961b639a836487e523e583b
Hugo Kart <hugo.hc@huauchinango.tecnm.mx>
2022-11-29 15:31:19 -0600

    Practica Info
    Cargando html e imágenes

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('ul.tabs li a:first').addClass('active');
+    $('sections article').hide();
+    $('sections article:first').show();
+
+    $('ul.tabs li a').click(function(){
+        $('ul.tabs li a').removeClass('active');
+        $(this).addClass('active');
+        $('.sections article').hide();
+
+        var activeTab = $(this).attr('href');
+        $(activeTab).show();
+        return false;
+    });
+});
